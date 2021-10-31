676031c0239de6da2e315c02a427f49dc48e5c2c
Alex <alexl@checkpay.ca>
2021-10-30 17:33:19 -0700

    Update Order.cs
    add public

--- ch6/6-3_start/HelloWord/Order.cs
+++ ch6/6-3_start/HelloWord/Order.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HelloWord
 {
-    class Order
+    public class Order
     {
         public int Id { get; set; }
         public DateTime DatePlaced { get; set; }
