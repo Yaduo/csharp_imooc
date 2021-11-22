d82f7432fcc36d28cedfac92c3721acdc783caa0
Alex <alexl@checkpay.ca>
2021-11-22 13:09:42 -0800

    ch7

---
+++ ch7/7-4_end/HelloWord/DiscountCalculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace HelloWord
+{
+    public class DiscountCalculator<T> where T : Product
+    {
+
+    }
+
+    // where T : IComparable
+    // where T : Product
+    // where T : struct
+    // where T : new()
+    public class Utility<T> where T : IComparable, new()
+    {
+        public int FindMax(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        public T FindMax(T a, T b)
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+
+        public void DoSomething()
+        {
+            var obj = new T();
+        }
+    }
+
+    public class Nullable<T> where T : struct
+    {
+        public Nullable() { }
+
+        private object _value;
+
+        public Nullable(T value)
+        {
+            _value = value;
+        }
+
+        public bool HasValue
+        {
+            get
+            {
+                return _value != null;
+            }
+        }
+
+        public T GetValueOrDefault()
+        {
+            if (HasValue)
+            {
+                return (T)_value;
+            }
+            return default(T);
+        }
+    }
+}
+
---
+++ ch7/7-4_end/HelloWord/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HelloWord
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var numberlist = new List();
+            var numberlist = new GenericList<int>();
+            numberlist.Add(1);
+            numberlist.Add(2);
+
+            //var productList = new ProductList();
+            var productList = new GenericList<Product>();
+            productList.Add(new Product()
+            {
+                Id = 1, 
+                Price = 100
+            });
+            productList.Add(new Product()
+            {
+                Id = 2,
+                Price = 200
+            });
+            
+            var dic = new System.Collections.Generic.Dictionary<string, Product>();
+            dic.Add("123", new Product());
+            dic.Add("234", new Product());
+            dic.Add("345", new Product());
+
+            Product product;
+            dic.TryGetValue("123", out product);
+
+            Console.Read();
+        }
+    }
+}
