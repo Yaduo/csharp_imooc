e5dc5db16307d3d56f0983972d1b00e8d3fedd2c
Yaduo Liu <yaduol@hotmail.com>
2021-12-17 13:15:31 -0800

    8-4

---
+++ ch8/8-4/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Timers;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+
+            Console.Read();
+        }
+    }
+}
